a330ad4297e38839ad6006425aff4e9cdb0b613c
Diego Maia <diegocmsantos@gmail.com>
2021-01-28 18:25:15 +0000

    Add tests for editMyProfile mutation

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -102,9 +102,42 @@ const a_user_calls_getMyProfile = async (user) => {
       return profile
 }
 
+const a_user_calls_editMyProfile = async (user, input) => {
+    const editMyProfile = `mutation editMyProfile($input: ProfileInput!) {
+        editMyProfile(newProfile: $input) {
+          bio
+          birthdate
+          createdAt
+          followersCount
+          followingCount
+          id
+          location
+          name
+          profileBackgroundImageUrl
+          profileImageUrl
+          screenName
+          tweetsCount
+          website
+          likesCount
+        }
+      }`
+
+      const variables = {
+          input
+      }
+
+      const data = await GraphQL(process.env.API_URL, editMyProfile, variables, user.accessToken)
+      const profile = data.editMyProfile
+
+      console.log(`[${user.username}] - edited profile`)
+
+      return profile
+}
+
 module.exports = {
     we_invoke_confirmUserSignup,
     a_user_signs_up,
     we_invoke_an_appsync_template,
-    a_user_calls_getMyProfile
+    a_user_calls_getMyProfile,
+    a_user_calls_editMyProfile
 }
